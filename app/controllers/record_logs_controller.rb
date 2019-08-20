6169979d3f14a137fc1063e407da50fed12095c7
vinicius ishizaki <vinicius.ishizaki@gmail.com>
2019-08-20 18:12:01 -0300

    included margin bottom on controle de registros

--- app/controllers/record_logs_controller.rb
+++ app/controllers/record_logs_controller.rb
@@ -1,5 +1,5 @@
 class RecordLogsController < ApplicationController
   def index
-    @record_logs = RecordLog.all.order("date DESC")
+    @record_logs = RecordLog.all.order("date DESC")[0..30]
   end
 end
