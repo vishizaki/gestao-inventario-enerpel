d7e32d0bf01b0ea44fcf88356d65c5370b982d85
vinicius ishizaki <vinicius.ishizaki@gmail.com>
2019-08-20 10:43:25 -0300

    corrected double items on inventory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 puts "Deleting Product, Inventory, Raw Material and logs"
 Product.delete_all
 Inventory.delete_all #deletar depois
@@ -16,26 +8,21 @@ RecordLog.delete_all #deletar depois
 def create_product(product_hash, final_product)
   
   product_hash.each do |product_name, product_quantity|
-    if product_name == RawMaterial.find_by(name: product_name)
+    if RawMaterial.find_by(name: product_name)
       material = RawMaterial.find_by(name: product_name)
     else
       material = RawMaterial.create!(name: product_name)
-
-      inventory = Inventory.new(name: material.name, quantity: 100)
-      inventory.raw_material = material
-      inventory.save
-      # binding.pry
-      # if inventory.raw_material.name == RawMaterial.find_by(name: material.name).name
-      #   Inventory.find(inventory.id).destroy
-      # end
+      unless Inventory.find_by(name: product_name)
+        inventory = Inventory.new(name: material.name, quantity: 100)
+        inventory.raw_material = material
+        inventory.save
+      end
     end
 
     product = Product.new(name: "#{final_product.name} #{product_name}", quantity: product_quantity)
     product.final_product = final_product
     product.raw_material = material
     product.save
-
-    # validate_material = RawMaterial.find_by(name: material.name)
   end
 end
 
@@ -92,28 +79,3 @@ create_product(product_grampo_portico_14a15_hash, final_product_grampo_portico_1
 final_product_grampo_portico_16a17 = FinalProduct.create!(name: "Grampo Guia de Descida em Pórtico - (16:00 a 17:99)")
 product_grampo_portico_16a17_hash = {"Suporte para BAP":1,"Grampo Guia de Descida (16:00 a 17:99)":1,"Parafuso M12x150":1,"Arruela Lisa M12":2,"Arruela Pressão M12":2,"Porca M12":3}
 create_product(product_grampo_portico_16a17_hash, final_product_grampo_portico_16a17)
-
-
-puts "Creating inventory"    
-# raw_material_list = ["AbraçadeiraNRX14","Arruela de Pressão M12","Arruela Lisa M12","Arruela Pressão M12","Bandejas Plasticas","Base para Fixação das Bandejas","Corpo da Caixa","Cruzeta","Cupilha Inox ","Fita Isolante","Grampo Guia de Descida (12:00 a 13:99)","Grampo Guia de Descida (14:00 a 15:99)","Grampo Guia de Descida (16:00 a 17:99)","Grampo Guia de Descida (9:00 a 11:99)","Lacre para kit entrada OPGW","O´Ring chato","O´Ring da caixa","O´Ring do lacre","O´Ring redondo","Parafuso Inox M5x16","Parafuso M12x150","Parafuso M12x65","Parafuso Olhal","Pino","Porca de Segurança","Porca M12","Protetores de Emenda","Suporte Externo","Suporte Interno","Suporte para BAP","Suporte T","Tampa da Caixa"]
-raw_material_list = RawMaterial.all
-raw_material_list.each do |raw_material|
-  inventory = Inventory.new(quantity: 100)
-  inventory.raw_material = raw_material
-  inventory.save
-end
-
-
-# product_opgw_hash.each do |product_name, product_quantity|
-#   material = RawMaterial.create!(name: product_name)  
-#   product = Product.new(name: "Caixa OPGW #{product_name}", quantity: product_quantity)
-#   product.final_product = final_product_opgw
-#   product.raw_material = material
-#   product.save
-  
-#   # puts "Creating inventory"
-#   inventory = Inventory.new(quantity: 100)
-#   inventory.raw_material = material
-#   inventory.save
-# end
-
