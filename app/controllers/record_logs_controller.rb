3ea04d894d20ddc26942ddb6f10404408c57ca19
vinicius ishizaki <vinicius.ishizaki@gmail.com>
2019-08-20 18:09:08 -0300

    implemented desc order on record logs

--- app/controllers/record_logs_controller.rb
+++ app/controllers/record_logs_controller.rb
@@ -1,5 +1,5 @@
 class RecordLogsController < ApplicationController
   def index
-    @record_logs = RecordLog.all.sort_by { |record| record.date }[1..30]
+    @record_logs = RecordLog.all.order("date DESC")
   end
 end
